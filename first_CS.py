af2b74df1db96322eedd2d01e206945b3efb53d7
MasoudiYosef <83264279+MasoudiYosef@users.noreply.github.com>
2021-06-09 11:09:06 +0430

    Delete first_CS.py

--- first_CS.py
+++
@@ -1,6 +0,0 @@
-def first_CS(noc,B):
-    import numpy as np
-    positions=np.random.randint(20,size=(noc,3))+np.random.rand(noc,3)
-    ligand_branches=np.random.randint(50,size=(noc,B))
-    first_p=np.hstack((positions,ligand_branches))
-    return first_p
